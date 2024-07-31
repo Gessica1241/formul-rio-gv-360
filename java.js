70b5ab6a008601cc0466f024b372fe4d759b1084
Gessica1241 <agencia.gv360@gmail.com>
2024-07-31 20:47:59 -0300

    correto agr kkkkkkkkkkk

--- java.js
+++ java.js
@@ -1,40 +1,30 @@
+function sendToWhatsApp() {
+    const nome = document.getElementById('nome').value;
+    const email = document.getElementById('email').value;
+    const msg = document.getElementById('msg').value;
+    const numeroWhatsApp1 = document.getElementById('numeroWhatsApp1').value;
 
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Formulário de Contato</title>
-    <link rel="stylesheet" href="script.js">
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
+
+        if (!nome || !email || !msg || !numeroWhatsApp1) {
+        alert('Por favor, preencha todos os campos.');
+        return;
+    }
     
-    <form id="contactForm">
-        <h1 style="font-family: Arial, sans-serif; 
-        font-size: 25px;">Faça seu agendamento:</h1>
-        <div class="form-group">
-            <label for="nome">Nome:</label>
-            <input type="text" id="nome" name="nome" required>
-        </div>
-        <div class="form-group">
-            <label for="email">Email:</label>
-            <input type="email" id="email" name="email" required>
-        </div>
-        <div class="form-group">
-            <label for="numeroWhatsApp1">Número do WhatsApp:</label>
-            <input type="tel" id="numeroWhatsApp1" name="numeroWhatsApp" placeholder="Ex: 5591999999999" required>
-        </div>
-        <div class="form-group">
-            <label for="msg">Mensagem:</label>
-            <input type="text" id="msg" name="msg" required>
-        </div>
-        
-        <button    type="button" onclick="sendToWhatsApp()"> <img src="whats3.png" alt="Logo">Enviar no WhatsApp
-           
-        </button>
-    </form>
+    const mensagem = `*Solicitação de agendamento*:\n\nNome: ${nome}\nEmail: ${email}\nNúmero${numeroWhatsApp1}\nMensagem: ${msg}`;
 
-    <script src="script.js"></script>
-</body>
-</html>
+   
+    const mensagemEncoded = encodeURIComponent(mensagem);
+
+   
+    const numeroWhatsApp = '5547988831423'; //mude aqui o número do cliente
+
+      
+      const numeroFormatado = numeroWhatsApp.replace(/\D/g, '');
+      
+
+    
+    const url = `https://wa.me/${numeroWhatsApp}?text=${mensagemEncoded}`;
+
+    
+    window.open(url, '_blank');
+}
